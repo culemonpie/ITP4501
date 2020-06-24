f525ca759cee7e7b21faf018516dc018a5ea3b06
hsk <robert@fingerprint.com.hk>
2020-06-24 14:24:37 +0800

    Initial Commit

---
+++ app/src/main/java/com/exercise/ea4513/Statistics.java
@@ -0,0 +1,14 @@
+package com.exercise.ea4513;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class Statistics extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_statistics);
+    }
+}
